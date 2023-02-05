f47767e68037766a8287486ad76628dda80f8c95
Eliza Vel <38025336+elizavel@users.noreply.github.com>
2023-02-05 18:27:16 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('test', function(Request $request) {
-    return $request->all();
-})
+Route::get('/test', function(Request $request) {
+    echo 'You entered '.$request->test;
+});
